c6a310ccbaa8c70c0379b583e04d3807349a7304
AmitPrabhat <AmitPrabhat@192.168.1.7>
2021-05-05 08:31:38 +0530

    Base Structure

---
+++ course-services/src/main/java/com/amit/course/CourseServicesApplication.java
@@ -0,0 +1,14 @@
+package com.amit.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseServicesApplication.class, args);
+		System.out.println("Application started...");
+	}
+
+}
---
+++ course-services/src/test/java/com/amit/course/CourseServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
